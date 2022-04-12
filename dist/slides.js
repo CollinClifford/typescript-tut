6152fceffb9ecb71c735d8c9a41a0bd9e01ed309
Collin Clifford <83039749+CollinClifford@users.noreply.github.com>
2022-04-12 14:27:11 -0700

    Add files via upload

---
+++ dist/slides.js
@@ -0,0 +1,41 @@
+"use strict";
+// Typescript is an open source language and a superset of JavaScript
+//
+// Offers additional features to JavaScript including static types
+// Using types is completely optional
+// Compiles down to regular JS
+// Can be used for front-end JavaScript as well as backend with Node.js
+// Includes most features from ES6, ES7, (classes, arrow functions, etc)
+// Types from 3rd party libraries can be added with type definitions
+//
+// Dynamic vs Static Typing
+//
+// In dynamically typed languages, the types are associated with run-time
+// values and not named explicitly in your code
+//
+// In statically typed languages, you explicitly assign types to variables,
+// function parameters, return values, etc
+//
+// Static Examples: Java, C, C++, Rust, Go
+// Dynamic Exmamples: JavaScript, Python, Ruby, PHP
+//
+// Pros:
+// More Robust
+// Easily Spot Bugs
+// Predictability
+// Readability
+// Popular
+//
+// Cons:
+// More Code to Write
+// More to Learn
+// Required Compilation
+// Not True Static Typing
+//
+// Compiling Typescript
+// Typescript using .ts and .tsx extensions
+// TSC (Typescript Compiler) is used to compile .ts files down to JS
+// Can watch files and report errors at compile time
+// Many tool include TS compilation by default
+// Most IDEs have great support for TS
+// the tsconfig.json file is used to configure how TypeScript works.
